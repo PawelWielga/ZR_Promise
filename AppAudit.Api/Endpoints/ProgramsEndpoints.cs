753612deb34421164febb88481b106f39966a89b
PawelWielga <wielgapawel@gmail.com>
2025-08-16 18:39:01 +0200

    add request DTOs for programs endpoints

--- AppAudit.Api/Endpoints/ProgramsEndpoints.cs
+++ AppAudit.Api/Endpoints/ProgramsEndpoints.cs
@@ -2,6 +2,7 @@
 using AppAudit.Contracts.Programs.Commands;
 using AppAudit.Contracts.Programs.Queries;
 using MediatR;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AppAudit.Api.Endpoints;
 
@@ -12,16 +13,14 @@ public static class ProgramsEndpoints
         var group = app.MapGroup("/api/programs");
 
         // GET: lista programów
-        group.MapGet("", async (
-            IMediator mediator,
-            int page = 1,
-            int pageSize = 25,
-            string? search = null,
-            bool? requiresKey = null,
-            bool? hasKey = null
-        ) =>
+        group.MapGet("", async (IMediator mediator, [AsParameters] GetProgramsPageRequest request) =>
         {
-            var res = await mediator.Send(new GetProgramsPageQuery(page, pageSize, search, requiresKey, hasKey));
+            var res = await mediator.Send(new GetProgramsPageQuery(
+                request.Page,
+                request.PageSize,
+                request.Search,
+                request.RequiresKey,
+                request.HasKey));
             return Results.Ok(res);
         });
 
@@ -33,16 +32,16 @@ public static class ProgramsEndpoints
         });
 
         // PATCH: toggle wymaga licencji
-        group.MapPatch("/{id:guid}/requires-license", async (IMediator mediator, Guid id, bool requires) =>
+        group.MapPatch("/{id:guid}/requires-license", async (IMediator mediator, Guid id, SetRequiresLicenseRequest request) =>
         {
-            await mediator.Send(new SetRequiresLicenseCommand(id, requires));
+            await mediator.Send(new SetRequiresLicenseCommand(id, request.Requires));
             return Results.NoContent();
         });
 
         // PUT: ustawienie/wyczyszczenie klucza licencji
-        group.MapPut("/{id:guid}/license-key", async (IMediator mediator, Guid id, string? key) =>
+        group.MapPut("/{id:guid}/license-key", async (IMediator mediator, Guid id, SetLicenseKeyRequest request) =>
         {
-            await mediator.Send(new SetLicenseKeyCommand(id, key));
+            await mediator.Send(new SetLicenseKeyCommand(id, request.Key));
             return Results.NoContent();
         });
 
